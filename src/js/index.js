87822229edb6a26ac33645acdd7f3e903f5686fd
puesyo <csanc236@fiu.edu>
2020-09-25 22:22:32 +0000

    assignment 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,32 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  // create variables
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // initialize variable with the content of H1 from index.html
+  let newSentence = document.querySelector("#myH1");
+
+  // generate a random number
+  function getRandomInt(maxIndex) {
+    return Math.floor(Math.random() * Math.floor(maxIndex));
+  }
+
+  // display new sentence in index.html
+  newSentence.innerHTML =
+    who[getRandomInt(who.length)] +
+    " " +
+    action[getRandomInt(action.length)] +
+    " " +
+    what[getRandomInt(what.length)] +
+    " " +
+    when[getRandomInt(when.length)];
 };
